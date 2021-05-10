f741e3f90200cb981928e39567fe644e13614668
Caro <cfouquet.web@gmail.com>
2021-05-10 22:48:32 +0200

    Update SeedData.cs to change music list

--- MvcMusic/Models/SeedData.cs
+++ MvcMusic/Models/SeedData.cs
@@ -23,40 +23,40 @@ namespace MvcMusic.Models
                 context.Music.AddRange(
                     new Music
                     {
-                        Title = "When Harry Met Sally OST",
-                        Artist =" ",
-                        ReleaseDate = DateTime.Parse("1989-2-12"),
-                        Genre = "OST",
+                        Title = "Cross",
+                        Artist ="Justice",
+                        ReleaseDate = DateTime.Parse("2007-6-11"),
+                        Genre = "Electro",
                         Price = 7.99M,
                         Rating = "R"
                     },
 
                     new Music
                     {
-                        Title = "Ghostbusters OST",
-                        Artist = " ",
-                        ReleaseDate = DateTime.Parse("1984-3-13"),
-                        Genre = "OST",
+                        Title = "Alive 2007",
+                        Artist = "Daft Punk",
+                        ReleaseDate = DateTime.Parse("2007-6-14"),
+                        Genre = "House",
                         Price = 8.99M,
                         Rating = "R"
                     },
 
                     new Music
                     {
-                        Title = "Ghostbusters 2 OST",
-                        Artist = " ",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
-                        Genre = "OST",
+                        Title = "Queen",
+                        Artist = "Queen",
+                        ReleaseDate = DateTime.Parse("1973-7-13"),
+                        Genre = "Rock",
                         Price = 9.99M,
                         Rating = "R"
                     },
 
                     new Music
                     {
-                        Title = "Rio Bravo OST",
-                        Artist = " ",
-                        ReleaseDate = DateTime.Parse("1959-4-15"),
-                        Genre = "OST",
+                        Title = "Liberté Chérie",
+                        Artist = "Calogero",
+                        ReleaseDate = DateTime.Parse("2017-8-25"),
+                        Genre = "Pop, Rock",
                         Price = 3.99M,
                         Rating = "R"
                     }
